3c0e6bf20c42ce87d68fd8eeadd4b6ca81f16885
AndresLince <lynxdarmatron@gmail.com>
2023-07-26 16:22:13 -0500

    Add getRateData functionality

--- lib/services/exchange.dart
+++ lib/services/exchange.dart
@@ -1,5 +1,13 @@
+import 'package:bitcoin_ticker_flutter/services/networking.dart';
+
+const apiKey = 'C2EBEA0A-1ED6-4F08-9BB6-503D00E51168';
+const coinApiUrl = 'https://rest.coinapi.io/v1';
+
 class ExchangeModel {
   Future<dynamic> getRateData() async {
-
+    final uri = Uri.parse(
+        '$coinApiUrl/exchangerate/BTC/USD?apikey=$apiKey');
+    NetworkHelper networkHelper = NetworkHelper(url: uri);
+    return networkHelper.getData();
   }
 }
